b2fb8ae1eee09d564e0c105a5dd96558b061052b
Nguyenkain <Nguyenkain@gmail.com>
2015-06-24 03:15:07 +0700

    Hoàn thiện dự án

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$(document).ready(function() {
+    var swiper = new Swiper('.slides', {
+        autoplay: 10000,
+        pagination: '.swiper-pagination',
+        autoplayDisableOnInteraction: false
+    });
+});
 
