11a67c81d90e452412a00e758cbee382b61ca42d
hugnnm <hungnm@newwave.vn>
2015-07-12 15:43:49 +0700

    6 page

--- js/main.js
+++ js/main.js
@@ -9,5 +9,21 @@ $(document).ready(function() {
         pagination: '.info-pagination',
         autoplayDisableOnInteraction: true
     });
+
 });
 
+
+//$(window).resize(function(){
+    if ($(window).width() <= 768){
+        $(document).ready(function() {
+            $('.content').insertBefore('.sidebar');
+        });
+    }
+
+    if ($(window).width() <= 480){
+        $(document).ready(function() {
+            $('.teacher-img-2').insertBefore('.teacher-info-2');
+            $('.share-img-2').insertBefore('.share-info-2');
+        });
+    }
+//});
